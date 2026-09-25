9e23f6e55228373124906e4da9fb85750734f9b0
JhonBlaxx <johnatan.lopez451@pascualbravo.edu.co>
2026-09-24 19:27:45 -0500

    Program.cs

--- Taller_1_Herramientas/Program.cs
+++ Taller_1_Herramientas/Program.cs
@@ -4,30 +4,39 @@ using Apifestivos.infraestructura.Datos;
 using Apifestivos.infraestructura.Repositorios;
 using Taller_1_Herramientas.Servicios;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<FestivosDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("FestivosConnection")));
-
-builder.Services.AddScoped<IPaisRepositorio, PaisRepositorio>();
-builder.Services.AddScoped<ITipoRepositorio, TipoRepositorio>();
-builder.Services.AddScoped<IFestivoRepositorio, FestivoRepositorio>();
-builder.Services.AddScoped<ICalendarioServicio, CalendarioServicio>();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+namespace Taller_1_Herramientas
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<FestivosDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("FestivosConnection")));
+
+            builder.Services.AddScoped<IPaisRepositorio, PaisRepositorio>();
+            builder.Services.AddScoped<ITipoRepositorio, TipoRepositorio>();
+            builder.Services.AddScoped<IFestivoRepositorio, FestivoRepositorio>();
+            builder.Services.AddScoped<ICalendarioServicio, CalendarioServicio>();
+
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
